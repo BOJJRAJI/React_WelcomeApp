aca66695b71e3a23fa643ad2e000b409f022b388
BOJJA RAJI <94085980+BOJJRAJI@users.noreply.github.com>
2023-09-13 18:20:24 +0530

    Update index.js

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -4,10 +4,8 @@ import './index.css'
 class Welcome extends Component {
   state = {isSubscribe: false}
 
-  subscribe = () => {
-    this.setState(prevState => ({
-      isSubscribe: !prevState.isSubscribe,
-    }))
+  onChangeState = () => {
+    this.setState(prevState => ({isSubscribe: !prevState.isSubscribe}))
   }
 
   render() {
@@ -15,14 +13,19 @@ class Welcome extends Component {
     return (
       <div className="bg-container">
         <h1 className="heading">Welcome</h1>
-        <p className="para">Thank you!Happy Learning</p>
-        <button type="button" className="button" onClick={this.subscribe}>
-          {isSubscribe ? 'Subscribed' : 'Subscribe'}
-        </button>
+        <p className="para">Thank you! Happy Learning</p>
+        {isSubscribe ? (
+          <button className="button" onClick={this.onChangeState} type="button">
+            Subscribed
+          </button>
+        ) : (
+          <button onClick={this.onChangeState} className="button" type="button">
+            Subscribe
+          </button>
+        )}
       </div>
     )
   }
 }
 
 export default Welcome
-
